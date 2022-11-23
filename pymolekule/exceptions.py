b8811c3c5eebd9adb6fa31f2bcb91b7bdb5f8920
Anthony Da Mota <AkdM@users.noreply.github.com>
2022-11-23 23:23:23 +0100

    docs: added exceptions file

--- pymolekule/exceptions.py
+++ pymolekule/exceptions.py
@@ -4,7 +4,11 @@ Pymolekule exceptions
 
 
 class PMAwsSrpAuthError(Exception):
-    """Exception raised when AWS SRP credentials are incorrect"""
+    """
+    Exception
+
+    Exception raised when AWS SRP credentials are incorrect
+    """
 
     def __init__(self):
         super().__init__('Unable to authenticate user for AWS SRP, check credentials')
